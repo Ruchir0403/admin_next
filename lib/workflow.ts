3774ef23fb3f703f2c0c1932b79d53bd1b02a5aa
ruchir0043 <kumarruchir194@gmail.com>
2025-01-18 22:24:26 +0530

    workflows for suer onboarding

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,35 @@
-import { Client as WorkFlowClient} from "@upstash/workflow"
-import config from "./config"
+import { Client as WorkFlowClient } from "@upstash/workflow";
+import config from "@/lib/config";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkFlowClient({
-    baseUrl: config.env.upstash.qstashUrl,
-    token: config.env.upstash.qstashToken,
-})
+  baseUrl: config.env.upstash.qstashUrl,
+  token: config.env.upstash.qstashToken,
+});
+
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Acme <ruchirkumar.work.gd>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
